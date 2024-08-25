80f16e97ff6c713f6ae2abcda163eac0fbcd1343
pete <153457778+ptraunf@users.noreply.github.com>
2024-08-25 17:23:43 -0400

    rm'd filter tests from main_test

--- main_test.go
+++ main_test.go
@@ -18,22 +18,7 @@ func (mrw mockResponseWriter) WriteHeader(status int) {}
 func (mrw mockResponseWriter) getData() []byte {
 	return mrw.Bytes()
 }
-func TestFilterHeaders(t *testing.T) {
 
-}
-func TestFilterRequest(t *testing.T) {
-
-}
-func TestAugmentRequest(t *testing.T) {
-
-}
-
-func TestFilterResponse(t *testing.T) {
-
-}
-func TestAugmentResponse(t *testing.T) {
-
-}
 func TestHandleTunnel(t *testing.T) {
 	// w := mockResponseWriter{}
 
